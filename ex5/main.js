81d59761c72c23d49fadcc6bde56be87405aa92c
morgan1000 <morgan.gardes.19@gmail.com>
2026-04-29 10:32:57 +0200

    Exercice 5 corrigé, ex6 en cours

--- ex5/main.js
+++ ex5/main.js
@@ -1,35 +1,15 @@
-// ================== EXERCICE 5 ==================== 
+// ================== EXERCICE 5 ====================
 let imagesRecup = document.querySelectorAll("img");
-// console.log(imagesRecup);
 
 imagesRecup.forEach((image) => {
   image.addEventListener("mouseover", handleMouseoverOnImage);
 });
 
-// function handleMouseoverOnImage(evenement) {
-//    let Mouseover = evenement.target;
-//   imagesRecup.src = "./images/image1_2.jpg";
-
 function handleMouseoverOnImage(evenement) {
-  let imageMouseover1 = evenement.target;
-  imageMouseover1.src = "./images/image1_2.jpg";
+  let iMouseover = evenement.target;
+  // console.log(iMouseover.src);
+
+  iMouseover.src = iMouseover.src.replace(".jpg", "_2.jpg");
+
+  iMouseover.removeEventListener("mouseover", handleMouseoverOnImage);
 }
-
-function handleMouseoverOnImage(evenement) {
-  
-}
-
-// function handleMouseoverOnImage(evenement) {
-//   let imageMouseover2 = evenement.target;
-//   imageMouseover2.src = "./images/image2_2.jpg";
-// }
-
-
-  // imagesRecup.src = "./images/image2_2.jpg";
-  // imagesRecup.src = "./images/image3_2.jpg";
-  // imagesRecup.src = "./images/image4_2.jpg";
-  // imagesRecup.src = "./images/image5_2.jpg";
-
-  // console.log(imagesRecup.src);
- 
-// }
--- ex6/main.js
+++ ex6/main.js
@@ -1,15 +1,21 @@
 // ========== EXERCICE 6 =============
-let imagesRecup = document.querySelectorAll("img")
-// console.log(imagesRecup);
+let imagesRecup = document.querySelectorAll("img");
 
- image.addEventListener("mouseover", handleMouseoverOnImage);
+imagesRecup.forEach((image) => {
+  image.addEventListener("mouseover", handleMouseoverOnImage);
+});
 
+function handleMouseoverOnImage(evenement) {
+  let iMouseover = evenement.target;
+//   console.log(iMouseover.src);
+  iMouseover.src = iMouseover.src.replace(".jpg", "_2.jpg");
+}
 
+imagesRecup.forEach((image) => {
+  image.addEventListener("mouseout", handleMouseoverOnImage);
+});
 
-
-
-
-
-
-
-//   image.addEventListener("mouseout", handleMouseoverOnImage);
+function handleMouseoverOnImage(evenement) {
+     let iMouseover2 = evenement.target;
+      iMouseover2.src = iMouseover2.src.replace("_2.jpg", ".jpg");
+}
